403d7265bff7517bce120baa74a660de51bfffe8
pramod <pramod.nandy06@gmail.com>
2021-04-13 09:07:31 +0200

    Update FindIfWithBoost.cpp
    Used Below editor:-
    https://wandbox.org/

--- FindIfWithBoost.cpp
+++ FindIfWithBoost.cpp
@@ -20,9 +20,9 @@ bool operator()(const Node & data)
 };
 int main() 
 {
-    std::vector<int> vec1 = {1, 2, 3, 4, 5};
-    std::vector<int> vec2 = {11, 12, 13, 14, 15};
-    std::vector<Node> input = {{0,12},{3,15},{4,10},{6,16},{7,17}};
+    const std::vector<int> vec1 = {1, 2, 3, 4, 5};
+    const std::vector<int> vec2 = {11, 12, 13, 14, 15};
+    const std::vector<Node> input = {{0,12},{3,15},{4,10},{6,16},{7,17}};
     for (const auto & zipped : boost::combine(vec1, vec2)) {
         //int i;int j; 
         //boost::tie(i,j) = zipped;
@@ -30,7 +30,7 @@ int main()
         
         std::cout << boost::get<0>(zipped) <<std::endl;
         std::cout << boost::get<1>(zipped) <<std::endl;
-        auto  it = std::find_if (
+        const auto  it = std::find_if (
                     input.begin(), 
                     input.end(), 
                     ContainsData(boost::get<0>(zipped), boost::get<1>(zipped)));
